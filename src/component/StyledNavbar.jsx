64501140c57de0e14186f4225b62000c0fecdec8
crossroads30 <crossroads30@mail.ru>
2024-01-09 15:34:03 +0300

    feat: add adding active classes examples

---
+++ src/component/StyledNavbar.jsx
@@ -0,0 +1,31 @@
+import { NavLink } from 'react-router-dom'
+
+const StyledNavbar = () => {
+	return (
+		<nav className='navbar'>
+			{/* styles for active link with inline styles */}
+			<NavLink
+				to='/'
+				style={({ isActive }) => {
+					return { color: isActive ? 'gold' : 'gray' }
+				}}
+			>
+				Home
+			</NavLink>
+			{/* styles for active link with adding css classes */}
+			<NavLink
+				to='about'
+				className={({ isActive }) => (isActive ? 'link active' : 'link')}
+			>
+				About
+			</NavLink>
+			<NavLink
+				to='products'
+				className={({ isActive }) => (isActive ? 'link active' : 'link')}
+			>
+				Product
+			</NavLink>
+		</nav>
+	)
+}
+export default StyledNavbar
--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../component/Navbar'
+import StyledNavbar from '../component/StyledNavbar'
 
 //stuff of this component will be inner structure of 'home page', but the own content of home page will be in home page component 
 const Home = () => {
 	return (
 		<>
-			<Navbar />
+			<StyledNavbar />
 			{/* this will always present in all pages */}
 			<Outlet />
 			{/* all nested pages */}
