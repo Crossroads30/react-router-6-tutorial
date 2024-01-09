cdb20a4ab6ae1a1ce7f3275e0c1b7f59fd6232e8
crossroads30 <crossroads30@mail.ru>
2024-01-09 16:15:23 +0300

    feat: add div

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,16 +1,17 @@
 import { Link, useParams } from 'react-router-dom'
 import products from '../data'
 
-
 const SingleProduct = () => {
 	const { productId } = useParams()
-  const product = products.find(product => product.id === productId)
-  const {name, image} = product
+	const product = products.find(product => product.id === productId)
+	const { name, image } = product
 
 	return (
 		<section className='section product'>
-			<h5>{name}</h5>
-      <img src={image} alt={name} />
+			<div>
+				<h5>{name}</h5>
+				<img src={image} alt={name} />
+			</div>
 			<Link to='/products'>back to product</Link>
 		</section>
 	)
