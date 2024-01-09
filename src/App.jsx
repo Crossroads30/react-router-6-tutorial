7f009dbc0858ed56682b957f899a20c5ded6de2a
crossroads30 <crossroads30@mail.ru>
2024-01-09 14:14:02 +0300

    feat: add error page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import About from './pages/About'
 import Products from './pages/Products'
 import Home from './pages/Home'
+import Error from './pages/Error'
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 				<Route path='/' element={<Home />} />
 				<Route path='about' element={<About />} />
 				<Route path='Products' element={<Products />} />
+				<Route path='*' element={<Error />} />
 			</Routes>
 		</BrowserRouter>
 	)
